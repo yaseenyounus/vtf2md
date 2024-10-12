088f1221a7efa8b2b57b0d1f01232d416ec74574
Yaseen Younus <yay200@gmail.com>
2024-10-12 11:38:25 -0500

    Adding var type to variables.tf example

--- variables.tf
+++ variables.tf
@@ -1,27 +1,33 @@
 variable "vpc_cidr_block" {
+  type = string
   description = "The top-level CIDR block for the VPC."
   default     = "10.1.0.0/16"
 }
 
 variable "cidr_blocks" {
+  type = list(string)
   description = "The CIDR blocks to create the workstations in."
   default     = ["10.1.1.0/24", "10.1.2.0/24"]
 }
 
 variable "namespace" {
+  type = string
   description = "Default namespace"
 }
 
 variable "cluster_id" {
+  type = string
   description = "Id to assign the new cluster"
 }
 
 variable "public_key_path" {
+  type = string
   description = "Path to public key for ssh access"
   default     = "~/.ssh/id_rsa.pub"
 }
 
 variable "node_groups" {
+  type = number
   description = "Number of nodes groups to create in the cluster"
   default     = 3
 }
