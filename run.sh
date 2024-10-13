90c290a94b4530af8ea638e80543d48fea8e5e8c
Yaseen Younus <yay200@gmail.com>
2024-10-13 09:50:33 -0500

    Cleaning up run.sh file

--- run.sh
+++ run.sh
@@ -1,11 +1,4 @@
 #!/bin/sh
 
-if ! command -v pip &> /dev/null; then
-    echo "pip isn't installed. Please install Python and try again. Exiting..."
-    exit 1
-fi
-
-pip install -q poetry
-
 poetry install
 poetry run python3 vtf2md/main.py "$@"
