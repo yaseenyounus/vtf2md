55772b34dc831d9ba7d82f7a1fd88fa18d29d0de
Yaseen Younus <yay200@gmail.com>
2024-10-12 21:41:01 -0500

    Adding main and helper functions

--- vtf2md/main.py
+++ vtf2md/main.py
@@ -3,34 +3,52 @@ from pytablewriter import MarkdownTableWriter
 from pytablewriter.style import Style
 
 
-md_table = []
-
-with open("variables.tf", "r") as file:
-    tf_dict = hcl2.load(file)
-
-for x in tf_dict["variable"]:
-    for key, value in x.items():
-        name = key
-        var_type = value.get("type", "").strip("${}")
-        description = value.get("description", "")
-        default = value.get("default", "n/a")
-
-        md_table.append(
-            [name, var_type, description, default, "True" if default == "n/a" else "False"]
-        )
+def load_tf_file(file_path: str) -> dict:
+    with open(file_path, "r") as file:
+        return hcl2.load(file)
 
 
-writer = MarkdownTableWriter(
-    headers=["Name", "Type", "Description", "Default", "Required"],
-    value_matrix=md_table,
-    margin=1,
-    column_styles=[
-        Style(align="left"),
-        Style(align="left"),
-        Style(align="left"),
-        Style(align="left"),
-        Style(align="left"),
-    ],
-)
+def extract_values(values: dict) -> list:
+    md_table = []
+    for x in values["variable"]:
+        for key, value in x.items():
+            name = key
+            var_type = value.get("type", "").strip("${}")
+            description = value.get("description", "")
+            default = value.get("default", "n/a")
 
-writer.write_table()
+            md_table.append(
+                [
+                    name,
+                    var_type,
+                    description,
+                    default,
+                    "True" if default == "n/a" else "False",
+                ]
+            )
+    return md_table
+
+
+def generate_md_table(values: list) -> None:
+    MarkdownTableWriter(
+        headers=["Name", "Type", "Description", "Default", "Required"],
+        value_matrix=values,
+        margin=1,
+        column_styles=[
+            Style(align="left"),
+            Style(align="left"),
+            Style(align="left"),
+            Style(align="left"),
+            Style(align="left"),
+        ],
+    ).write_table()
+
+
+def main():
+    terraform_dict = load_tf_file("variables.tf")
+    markdown_list = extract_values(terraform_dict)
+    generate_md_table(markdown_list)
+
+
+if __name__ == "__main__":
+    main()
