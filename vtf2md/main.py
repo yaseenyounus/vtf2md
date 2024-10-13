cbd5631aa2d6a6bff238c1a1534021066435598d
Yaseen Younus <yay200@gmail.com>
2024-10-12 21:08:53 -0500

    Python data to markdown table

--- vtf2md/main.py
+++ vtf2md/main.py
@@ -1,6 +1,11 @@
 import hcl2
+from pytablewriter import MarkdownTableWriter
+from pytablewriter.style import Style
 
-with open("variables.tf", 'r') as file:
+
+md_table = []
+
+with open("variables.tf", "r") as file:
     tf_dict = hcl2.load(file)
 
 for x in tf_dict["variable"]:
@@ -10,4 +15,26 @@ for x in tf_dict["variable"]:
         description = value.get("description", None)
         default = value.get("default", None)
 
-        print(name, var_type,  description, default)
+        md_table.append(
+            [name, var_type, description, default, "False" if default else "True"]
+        )
+
+        print(name, var_type, description, default)
+
+print(md_table)
+
+
+writer = MarkdownTableWriter(
+    headers=["Name", "Type", "Description", "Default", "Required"],
+    value_matrix=md_table,
+    margin=1,
+    column_styles=[
+        Style(align="left"),
+        Style(align="left"),
+        Style(align="left"),
+        Style(align="left"),
+        Style(align="left"),
+    ],
+)
+
+writer.write_table()
